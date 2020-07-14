d2c1de2adee96ffabd2ce36c84e118c633c9b29f
Katie <ksharples431@gmail.com>
2020-07-14 16:12:09 -0400

    author routes done

--- models/author.js
+++ models/author.js
@@ -3,10 +3,11 @@ let mongoose = require('mongoose');
 let Schema = mongoose.Schema;
 
 let AuthorSchema = new Schema({
-  first_name: { type: String, required: true },
-  last_name: { type: String, required: true },
+  first_name: { type: String, required: true, maxlength: 100 },
+  last_name: { type: String, required: true, maxlength: 100 },
 });
 
+// Virtual for author "full" name.
 AuthorSchema.virtual('name').get(function () {
   var fullname = '';
 
@@ -20,8 +21,10 @@ AuthorSchema.virtual('name').get(function () {
   return fullname;
 });
 
+// Virtual for this author instance URL.
 AuthorSchema.virtual('url').get(function () {
   return '/catalog/author/' + this._id;
 });
 
+// Export model.
 module.exports = mongoose.model('Author', AuthorSchema);
